5e78381dcc3c570542339ff301223fa5dc36477b
d-luan <dluan1285@gmail.com>
2025-12-10 18:10:01 -0300

    feat: homework assignment completed on variables

---
+++ CommonVariables/PersonInformation/PersonInformationApp/Program.cs
@@ -0,0 +1,40 @@
+﻿string name = "John Smith";
+int age = 23;
+bool isAlive = true;
+string phoneNumber = "+1 432-545-2873";
+
+Console.WriteLine("First person information...");
+Console.WriteLine();
+
+if (isAlive)
+{
+    Console.WriteLine($"Name: {name}");
+    Console.WriteLine($"Age: {age}");
+    Console.WriteLine($"Is alive? {isAlive}");
+    Console.WriteLine($"Phone number: {phoneNumber}");
+}
+else
+{
+    Console.WriteLine($"{name} is dead!");
+}
+
+Console.WriteLine();
+Console.WriteLine("Second person information...");
+Console.WriteLine();
+
+name = "Mary Taylor";
+age = 56;
+isAlive = false;
+phoneNumber = "+1 555-983-6213";
+
+if (isAlive)
+{
+    Console.WriteLine($"Name: {name}");
+    Console.WriteLine($"Age: {age}");
+    Console.WriteLine($"Is alive? {isAlive}");
+    Console.WriteLine($"Phone number: {phoneNumber}");
+}
+else
+{
+    Console.WriteLine($"{name} is dead!");
+}
