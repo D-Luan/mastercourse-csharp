317848d0bb9ba7d6c34a8b4899da259c5286da6f
d-luan <dluan1285@gmail.com>
2025-12-10 17:34:41 -0300

    feat: add learning about null variables

---
+++ CommonVariables/NullVariables/NullVariablesApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+// null - lack of value
+
+// Haven't asked for the age yet
+int? age = null;
+bool? birthday = null;
+double? battingAverage = null;
+decimal? accountBalance = null;
+string? firstName = null;
+
+// We have asked for the age now
+age = 0;
+
+double a = 0.1;
+double b = 0.2;
+Console.WriteLine(a + b == 0.3);
