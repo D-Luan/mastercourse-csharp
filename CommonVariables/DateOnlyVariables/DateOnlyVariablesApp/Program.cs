90f18e1d63a4c6ba19b1bd818e99bbae41467b57
d-luan <dluan1285@gmail.com>
2025-12-11 17:13:12 -0300

    feat: add learning about the DateOnly type

---
+++ CommonVariables/DateOnlyVariables/DateOnlyVariablesApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+DateTime today = DateTime.Now;
+
+DateOnly birthday = DateOnly.Parse("6/11/1998");
+
+Console.WriteLine(birthday.ToString("MMMM dd, yyyy"));
+
+Console.WriteLine($"Today full format: {today}");
+Console.WriteLine($"Today just date: {today.Date}");
+Console.WriteLine($"Birthday full format: {birthday}");
+
+DateOnly currentDate = DateOnly.FromDateTime(DateTime.UtcNow);
+Console.WriteLine(currentDate);
