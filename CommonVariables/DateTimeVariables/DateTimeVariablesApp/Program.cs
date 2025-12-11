781497e0dfbac5333190c58a8f24bb45a6cc1afb
d-luan <dluan1285@gmail.com>
2025-12-11 11:57:00 -0300

    feat: add learning about DateTime variables

---
+++ CommonVariables/DateTimeVariables/DateTimeVariablesApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+// Display current data for all cultures
+DateTime today = DateTime.Now;
+string formatedDate = today.ToString("d/MM/yyyy", CultureInfo.InvariantCulture);
+
+DateTime todayOnlyDate = today.Date;
+//DateTime birthday = DateTime.Parse("6/11/1998");
+
+//DateTime birthday = DateTime.ParseExact("6/11/1998", "d/M/yyyy", CultureInfo.InvariantCulture);
+
+Console.WriteLine(todayOnlyDate);
+
+Console.WriteLine(formatedDate);
+
+//Console.WriteLine(birthday);
+
+Console.WriteLine(DateTime.Now.ToString("MMMM dd, yyyy HH:mm tt zzz"));
