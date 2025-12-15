092d63f5668a10eeb03eb643d38699b9d9b4ffd2
d-luan <dluan1285@gmail.com>
2025-12-15 15:58:20 -0300

    feat: add homework solution about type conversions

---
+++ Homeworks/HomeworkTypeConversions/HomeworkTypeConversionsApp/Program.cs
@@ -0,0 +1,21 @@
+﻿bool ageIsValid;
+int age;
+bool ageValidation;
+
+do
+{
+	Console.Write("Enter your age: ");
+	string? ageText = Console.ReadLine();
+
+	ageIsValid = int.TryParse(ageText, out age);
+
+    ageValidation = !ageIsValid || (age < 0 || age > 120);
+
+    if (ageValidation)
+    {
+		Console.WriteLine("Invalid number! Please enter a valid number.");
+	}
+
+} while (ageValidation);
+
+Console.WriteLine($"You would be {age + 25} in 25 years, and 25 years ago, you were {age - 25}");
