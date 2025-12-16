a079713a4dc3d921729f2dc5585737cb94c21333
d-luan <dluan1285@gmail.com>
2025-12-16 17:16:20 -0300

    feat: add homework about conditional statements

---
+++ Homeworks/HomeworkConditionalStatements/HomeworkConditionalStatementsApp/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("What is your name? ");
+string? name = string.Empty;
+
+try
+{
+    name = Console.ReadLine();
+}
+catch
+{
+    Console.WriteLine("Invalid name!");
+}
+
+if (name.ToLower() == "tim") 
+{
+    Console.WriteLine($"Welcome Professor {name}!");
+}
+else
+{
+    Console.WriteLine($"Welcome student {name}!");
+}
