74f5d22ddbeb397159b216a9238358434d0b95a6
d-luan <dluan1285@gmail.com>
2025-12-18 16:45:57 -0300

    feat: add homework solution about do loops

---
+++ Homeworks/HomeworkDoLoops/HomeworkDoLoopsApp/Program.cs
@@ -0,0 +1,24 @@
+﻿
+string answer;
+
+do
+{
+    Console.Write("Enter your name: ");
+    string? firstName = Console.ReadLine();
+    
+    if (firstName.ToLower() == "tim")
+    {
+        firstName = $"Professor {firstName}";
+    }
+    else
+    {
+        firstName = $"Student {firstName}";
+    }
+
+    Console.WriteLine($"Welcome me {firstName}.");
+
+    Console.Write("Exit or Continue? ");
+    answer = Console.ReadLine();
+} while (answer.ToLower() != "exit");
+
+Console.WriteLine("End of Application");
