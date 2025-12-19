119c5e8152f56789b3c3f3f61d29562810e21e08
d-luan <dluan1285@gmail.com>
2025-12-19 17:37:48 -0300

    feat: add homework solution about array

---
+++ Homeworks/HomeworkArray/HomeworkArrayApp/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] firstName = new string[] { "Smith", "John", "Mary" };
+
+string? numberText = string.Empty;
+bool isValidNumber = false;
+int number = 0;
+
+do
+{
+    Console.Write("Enter a number to verify a name: ");
+    numberText = Console.ReadLine();
+
+    isValidNumber = int.TryParse(numberText, out number);
+
+    if (isValidNumber == false)
+    {
+        Console.WriteLine("Invalid Number. Enter a valid number.");
+    }
+
+    if (number > (firstName.Length - 1))
+    {
+        Console.WriteLine("Invalid number. Enter a number between 0 and 2.");
+    }
+    
+} while (isValidNumber == false || number > (firstName.Length - 1));
+
+Console.WriteLine($"Name stored in number {number}: {firstName[number]}");
